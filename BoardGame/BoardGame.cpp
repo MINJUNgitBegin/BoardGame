6a25d8b3f95c54a81415ef1b500af8d5318f42e3
user <user@DESKTOP-KNKAEGR>
2018-12-19 20:19:00 +0900

    vvvvvv

--- BoardGame/BoardGame.cpp
+++ BoardGame/BoardGame.cpp
@@ -9,6 +9,8 @@ int main()
     std::cout << "Hello World!\n";
 	std::cout << "World" << std::endl;
 	std::cout << "World" << std::endl;
+	std::cout << "world fuck" << std::endl;
+
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
