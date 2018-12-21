376b903f1f4f0e1762d19a017ba92c6fc5480bf6
user <user@DESKTOP-KNKAEGR>
2018-12-21 12:08:47 +0900

    sddasd

--- BoardGame/Event.cpp
+++ BoardGame/Event.cpp
@@ -7,15 +7,27 @@
 
 void BeDraggedAOG(Person* p)
 {
+	p->LocatedCell->RestCounterInAOG;
 	
+	//moveplayer의 값을 지정한 값으로 이동후 무인도 함수실행;
 }
 void UseLadder(Person* p)
 {
+	p->LocatedCell->//지정한값으로 보내기
 	
+	//moveplayer의 값을 지정한값으로 이동후
 }
 bool IsInGoal(const Person* p)
 {
-	
+	if (p->locatedCell->ReturnAttribute())
+	{
+		system("cls");
+		std::cout << " " << p->Number <<"you Win" << std::endl;
+	}
+	else
+	{
+		
+	}
 		
 
 	
@@ -25,17 +37,13 @@ void GenerateEvent(Person* p)
 	switch (p->locatedcell->ReturnAttribute())
 	{
 		case 1:
-			BeDraggedAOG();
+			BeDraggedAOG(p);
 			break;
 		case 2:
-			UseLadder();
+			UseLadder(p);
 			break;
 		default:
-			IsInGoal();
-		if(p->locatedC)
-		{
-				
-		}
+			IsInGoal(p);
 			break;
 	}
 	
